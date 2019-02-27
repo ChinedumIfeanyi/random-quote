012f778b0295d0745926e76f837612d576036e80
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-02-27 11:32:36 +0100

    Updates

---
+++ precache-manifest.e291b4c676bfc5ea9aa6f27bf86e3b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0be3ec75e707d124709",
+    "url": "/ChinedumIfeanyi/random-quote/static/js/runtime~main.e0be3ec7.js"
+  },
+  {
+    "revision": "a7a1be1086ab49812677",
+    "url": "/ChinedumIfeanyi/random-quote/static/js/main.a7a1be10.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/ChinedumIfeanyi/random-quote/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "a7a1be1086ab49812677",
+    "url": "/ChinedumIfeanyi/random-quote/static/css/main.8d623804.chunk.css"
+  },
+  {
+    "revision": "92cdde5e5fa5ce8b13176f76abe6ad49",
+    "url": "/ChinedumIfeanyi/random-quote/index.html"
+  }
+];
